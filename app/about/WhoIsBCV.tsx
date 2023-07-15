df086547628ca3fcea17346ca866715c5e2037b5
Philani Ncube <ncbphi001@gmail.com>
2023-07-15 13:02:45 +0200

    Who Is

--- app/about/WhoIsBCV.tsx
+++ app/about/WhoIsBCV.tsx
@@ -1,6 +1,29 @@
+import { Database } from "@/schema";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import Image from "next/image";
 
-const WhoIsBCV = () => {
+const images = [
+  "/images/children.jpg",
+  "/images/farmer.jpg",
+  "/images/aerial.jpg",
+  "/images/valley.jpg",
+]
+
+const WhoIsBCV = async () => {
+
+   const supabase = createServerComponentClient<Database>({ cookies });
+
+   const { data: directors, error } = await supabase
+     .from("directors")
+     .select("*")
+     .order("position", { ascending: true });
+
+   const { data: portfolios, error:portfolioErrors } = await supabase
+     .from("portfolios")
+     .select("*")
+     .order("title", { ascending: true });
+
   return (
     <div className="w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -23,7 +46,55 @@ const WhoIsBCV = () => {
           </p>
         </div>
         <div className="w-full">
-          <Image src="/images/directors.jpeg" width={1166} height={813} alt="Directors" className="w-full object-cover aspect-video" />
+          <Image
+            src="/images/directors.jpeg"
+            width={1166}
+            height={813}
+            alt="Directors"
+            className="w-full object-cover aspect-video"
+          />
+        </div>
+      </div>
+      <div className="mt-10 w-full">
+        <h1 className="text-2xl text-center font-bold">BCV Directors</h1>
+        <div className="flex flex-wrap justify-center gap-4 mt-4">
+          {directors?.map((director) => (
+            <div
+              key={director.id}
+              className="w-full max-w-[400px] flex flex-col p-4 justify-center text-center items-center space-y-2 bg-slate-200 rounded-md"
+            >
+              <h2 className="text-lg font-bold text-zinc-700">
+                {director.position}
+              </h2>
+              <p className="text-md text-slate-600">
+                {director.first_name} {director.last_name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16 w-full grid grid-cols-2 gap-6">
+        <div className="w-full">
+          <h2 className="text-2xl font-bold">BCV Portfolios</h2>
+          <div className="grid grid-cols-2 justify-between gap-4 mt-4 ">
+            {portfolios?.map((portfolio) => (
+              <article className="w-full border-b border-slate-300 pb-3">
+                <h3 className="font-semibold text-lg">{portfolio.title}</h3>
+                <p className="text-md mt-2">{portfolio.manager}</p>
+              </article>
+            ))}
+          </div>
+        </div>
+        <div className="w-full grid grid-cols-2 gap-3">
+          {images.map((image) => (
+            <Image
+              src={image}
+              width={1166}
+              height={813}
+              alt="Image"
+              className="w-full object-cover aspect-video rounded-md"
+            />
+          ))}
         </div>
       </div>
     </div>
