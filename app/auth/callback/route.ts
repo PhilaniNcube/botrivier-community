b74abba5bb2884d2dbd68d95f4d3cb0a03046aab
Philani Ncube <ncbphi001@gmail.com>
2023-07-15 09:24:04 +0200

    callback route handler

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,9 +3,7 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the Auth Helpers package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
+
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
 
@@ -14,6 +12,6 @@ export async function GET(request: Request) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // URL to redirect to after sign in process completes
+
   return NextResponse.redirect(requestUrl.origin)
 }
